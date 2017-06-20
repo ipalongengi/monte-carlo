329ae21ff480d0e24de03aeca837d95ef3c687fd
Ichwan Palongengi <ipalongengi@gmail.com>
2017-06-19 20:29:36 -0400

    Add the assignment docs and work on the project
    - Append the assignment documents as given by the professor
    - Create the method that generate normally-distributed random numbers
    - Create the method that put those random numbers into bins
    
    	new file:   asgmnt_docs/asgmnt2_p1.pdf
    	new file:   asgmnt_docs/asgmnt2_p2.pdf
    	modified:   src/MonteCarlo.java
    	modified:   src/Simulation.java

--- src/MonteCarlo.java
+++ src/MonteCarlo.java
@@ -1,5 +1,16 @@
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
+import java.util.ArrayList;
+
 public class MonteCarlo {
     public static void main(String args[]){
-
+        ArrayList<Double>randomNumbers = new ArrayList();
+        Simulation.generateNormalRandomNumbers(randomNumbers, 100);
+        System.out.println(randomNumbers);
+        System.out.println(randomNumbers.size());
+        System.out.println(Simulation.getMin(randomNumbers));
+        System.out.println(Simulation.getMax(randomNumbers));
+        System.out.println(Simulation.getRange(randomNumbers));
+        System.out.println(Simulation.makeBins(randomNumbers, 10));
     }
 }
--- src/Simulation.java
+++ src/Simulation.java
@@ -1,30 +1,51 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Collections;
+
 
 public class Simulation {
-    public void generateNormalRandomNumbers(ArrayList<Double> numArray, int num){
+//
+//    public Simulation(int inputSize){
+//        int size = inputSize;
+//        ArrayList<Double>numArray = new ArrayList();
+//    }
+
+    public static void generateNormalRandomNumbers(ArrayList<Double> numArray, int num){
+        Random randomGenerator = new Random();
+        for (int i = 0; i < num; i++){
+            numArray.add(i, randomGenerator.nextGaussian() * 100);
+        }
     }
 
-    public ArrayList<Double> makeBins (ArrayList<Double> numArray, int numBins){
-        return null;
+    public static int[] makeBins (ArrayList<Double> numArray, int numBins){
+        Double binRange = getRange(numArray) / numBins;
+        int buckets[] = new int[numBins];
+
+        for (int i = 0; i < numArray.size(); i++){
+            buckets[(int)(numArray.get(i)/binRange)] += 1;
+        }
+
+        return buckets;
     }
 
-    public int getBin(double randNum){
+//    public static int getBin(double randNum){
+//        return (int) (randNum/)
+//    }
+
+    public static int getBin(double randNum, double binSize, double minRandNum, double maxRandNum){
         return 0;
     }
 
-    public int getBin(double randNum, double binSize, double minRandNum, double maxRandNum){
-        return 0;
+    public static Double getMin(ArrayList<Double> numArray){
+        return Collections.min(numArray);
     }
 
-    private Double getMin(ArrayList<Double> numArray){
-        return 0.0;
+    public static Double getMax(ArrayList<Double> numArray){
+        return Collections.max(numArray);
     }
 
-    private Double getMax(ArrayList<Double> numArray){
-        return 0.0;
-    }
-
-    private Double getRange(ArrayList<Double> numArray){
-        return 0.0;
+    public static Double getRange(ArrayList<Double> numArray){
+        return getMax(numArray) - getMin(numArray);
     }
 }
