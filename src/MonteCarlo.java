a64dd00772949c46aa37b6e46b7f8c137a0bd1d9
Ichwan Palongengi <ipalongengi@gmail.com>
2017-06-23 21:16:36 -0400

    Make important additions to the `Simulation` class
    - make class fields private and final (i.e. once they are
      created *AND* initialized, the instantiated object is
      pretty much immutable)
    - add getter method `getArray()` to retrieve the `ArrayList`
      of randomly generated numbers
    - add getter method `getBuckets()` to retrieve the generated
      buckets and the count of random numbers that are within their
      range
    - override the built-in `toString()` method to give more useful
      information about the `Simulation` class
    - reorder class methods
    - instantiate a `Simulation` class in the `main()` method

--- src/MonteCarlo.java
+++ src/MonteCarlo.java
@@ -4,13 +4,7 @@ import java.util.ArrayList;
 
 public class MonteCarlo {
     public static void main(String args[]){
-        ArrayList<Double>randomNumbers = new ArrayList();
-        Simulation.generateNormalRandomNumbers(randomNumbers, 100);
-        System.out.println(randomNumbers);
-        System.out.println(randomNumbers.size());
-        System.out.println(Simulation.getMin(randomNumbers));
-        System.out.println(Simulation.getMax(randomNumbers));
-        System.out.println(Simulation.getRange(randomNumbers));
-        System.out.println(Simulation.makeBins(randomNumbers, 10));
+        Simulation sim = new Simulation(100, 20);
+        System.out.println(sim.toString());
     }
 }
--- src/Simulation.java
+++ src/Simulation.java
@@ -1,51 +1,73 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Collections;
 
 
 public class Simulation {
-//
-//    public Simulation(int inputSize){
-//        int size = inputSize;
-//        ArrayList<Double>numArray = new ArrayList();
-//    }
 
-    public static void generateNormalRandomNumbers(ArrayList<Double> numArray, int num){
+    private final ArrayList<Double>numArray;
+    private final int[] buckets;
+    private final Double binRange;
+
+    public Simulation(int numElements, int numBins){
+        numArray = new ArrayList<>(numElements);
+        generateNormalRandomNumbers(numArray, numElements);
+        buckets = makeBins(numArray, numBins);
+        binRange = getRange(numArray) / numBins;
+    }
+
+    private static void generateNormalRandomNumbers(ArrayList<Double> numArray, int num){
         Random randomGenerator = new Random();
         for (int i = 0; i < num; i++){
             numArray.add(i, randomGenerator.nextGaussian() * 100);
         }
     }
 
-    public static int[] makeBins (ArrayList<Double> numArray, int numBins){
+    private static int[] makeBins (ArrayList<Double> numArray, int numBins){
         Double binRange = getRange(numArray) / numBins;
         int buckets[] = new int[numBins];
+        Double minArray = getMin(numArray);
 
-        for (int i = 0; i < numArray.size(); i++){
-            buckets[(int)(numArray.get(i)/binRange)] += 1;
+        for (Double num: numArray){
+            int index = (int)((num-minArray)/binRange);
+//            System.out.printf("%f falls into bucket #%d\n", num, index);
+
+            if (index == numBins){
+                buckets[index - 1] += 1;
+            }
+            else{
+                buckets[index] += 1;
+            }
         }
-
         return buckets;
     }
 
-//    public static int getBin(double randNum){
-//        return (int) (randNum/)
-//    }
-
-    public static int getBin(double randNum, double binSize, double minRandNum, double maxRandNum){
-        return 0;
-    }
-
-    public static Double getMin(ArrayList<Double> numArray){
+    private static Double getMin(ArrayList<Double> numArray){
         return Collections.min(numArray);
     }
 
-    public static Double getMax(ArrayList<Double> numArray){
+    private static Double getMax(ArrayList<Double> numArray){
         return Collections.max(numArray);
     }
 
-    public static Double getRange(ArrayList<Double> numArray){
+    private static Double getRange(ArrayList<Double> numArray){
         return getMax(numArray) - getMin(numArray);
+    }
+
+    public int getBin(double randNum){
+        return (int) ((randNum - getMin(numArray)) / binRange);
+    }
+
+    public ArrayList<Double> getArray(){
+        return numArray;
+    }
+
+    public int[] getBuckets(){
+        return buckets;
+    }
+
+    public String toString(){
+        return String.format("Total Elements: %d, Num of Buckets: %d, Min: %f, Max: %f, Range: %f",
+                numArray.size(), buckets.length,  getMin(numArray), getMax(numArray), getRange(numArray));
     }
 }
