00208e74527b442d11bc3c3e0b108a04e583c61e
Ichwan Palongengi <ipalongengi@gmail.com>
2017-06-26 23:14:07 -0400

    Developed methods in `Histogram` class
    - make all existing fields private
    - add four more fields to store data originating
      from the Simulation object
    - simplify the `drawXAxis()` and `drawYAxis()` method
      by removing redundant variables
    - develop the `drawBins()` method which, well, actually
      draw the bins according to their frequencies
    - develop the `scaleY()` method which is used to fit the
      diagram according to the effective area available
    
     Changes to be committed:
    	deleted:    .swp
    	modified:   src/Histogram.java
    	modified:   src/MonteCarlo.java

--- src/Histogram.java
+++ src/Histogram.java
@@ -1,20 +1,30 @@
 import javax.swing.JPanel;
 import java.awt.*;
 import java.text.DecimalFormat;
+import java.util.Collections;
 
 public class Histogram extends JPanel{
-    final int TOP_MARGIN = 20;
-    final int BOTTOM_MARGIN = 20;
-    final int LEFT_MARGIN = 20;
-    final int RIGHT_MARGIN = 20;
+    private final int TOP_MARGIN = 20;
+    private final int BOTTOM_MARGIN = 20;
+    private final int LEFT_MARGIN = 20;
+    private final int RIGHT_MARGIN = 20;
 
-    // Declare instance variables here...
+    private final Double minRandNum;
+    private final int bins[];
+    private final int maxFreq;
+    private final Double arrayRange;
 
     // Constructor
     public Histogram(Simulation s){
         setBackground(Color.WHITE);
 
-        // declarations of instance variables
+        // Fields pertinent to the actual ArrayList of random numbers
+        minRandNum = Collections.min(s.getArray());
+        arrayRange = s.getRange();
+
+        // Fields pertinent to the bucket array
+        bins = s.getBuckets();
+        maxFreq = s.getMaxFreq();
     }
 
     // Draws the histogram
@@ -28,74 +38,85 @@ public class Histogram extends JPanel{
     }
 
     // Draws the x-axis
-    public void drawXAxis(Graphics g){
-        int x1 = LEFT_MARGIN;
+    private void drawXAxis(Graphics g){
         int y1 = getHeight() - BOTTOM_MARGIN;
         int x2 = getWidth() - RIGHT_MARGIN;
-        int y2 = y1;
-        g.drawLine(x1, y1, x2, y2);
+        g.drawLine(LEFT_MARGIN, y1, x2, y1);
     }
 
     // Draws the y-axis
-    public void drawYAxis(Graphics g){
-        int x1 = LEFT_MARGIN;
+    private void drawYAxis(Graphics g){
         int y1 = getHeight() - BOTTOM_MARGIN;
-        int x2 = x1;
-        int y2 = TOP_MARGIN;
-        g.drawLine(x1, y1, x2, y2);
+        g.drawLine(LEFT_MARGIN, y1, LEFT_MARGIN, TOP_MARGIN);
     }
 
     // Draws the bins
-    public void drawBins(Graphics g){
+    private void drawBins(Graphics g){
         g.setColor(Color.GRAY);
-        // Put additional code over here
+
+        // Find the optimal width for every bins from the available effective width of the frame
+        int bestWidth = (getWidth() - LEFT_MARGIN - RIGHT_MARGIN)/bins.length;
+
+        for (int i = 0; i < bins.length; i++){
+            g.drawRect((bestWidth * i) + LEFT_MARGIN,
+                    getHeight() - BOTTOM_MARGIN - scaleY(bins[i]),
+                    bestWidth,
+                    scaleY(bins[i]));
+            g.fillRect((bestWidth * i) + LEFT_MARGIN,
+                    getHeight() - BOTTOM_MARGIN - scaleY(bins[i]),
+                    bestWidth - 1,
+                    scaleY(bins[i]));
+        }
+
     }
 
     // Draws the labels along the x-axis
-    public void drawXLabels(Graphics g){
+    private void drawXLabels(Graphics g){
         g.setColor(Color.BLACK);
 
+        // Format the output of labels on the x-axis
+        // to only show up to 2 significant digits
         DecimalFormat formatter = new DecimalFormat();
         formatter.setMinimumFractionDigits(2);
         formatter.setMaximumFractionDigits(2);
 
-//        // Sample Code - might be useful
-//        double labelVal = min;
-//        String label = formatter.format(labelVal);
-//        int x = LEFT_MARGIN;
-//        int y = getHeight() - BOTTOM_MARGIN + 12;
-//        for (int b: bins){
-//            g.drawString(label, x - 12, y);
-//            x += binWidth;
-//            labelVal += binSize;
-//            label = formatter.format(labelVal);
-//        }
-//        g.drawString(label, x - 12, y);
+        int bestWidth = (getWidth() - LEFT_MARGIN - RIGHT_MARGIN)/bins.length;
+
+        // Sample Code - might be useful
+        double labelVal = minRandNum;
+        String label = formatter.format(labelVal);
+        int x = LEFT_MARGIN;
+        int y = getHeight() - BOTTOM_MARGIN + 12;
+        for (int ignored : bins){
+            g.drawString(label, x - 12, y);
+            x += bestWidth;
+            labelVal += arrayRange;
+            label = formatter.format(labelVal);
+        }
+        g.drawString(label, x - 12, y);
     }
 
     // Draws the labels along the y-axis
-    public void drawYLabels(Graphics g){
+    private void drawYLabels(Graphics g){
         g.setColor(Color.BLUE);
 
-        DecimalFormat formatter = new DecimalFormat();
-        formatter.setMinimumFractionDigits(2);
-        formatter.setMaximumFractionDigits(2);
+        DecimalFormat formatter = new DecimalFormat("#,###");
 
-//        // Sample Code - might be useful
-//        double labelVal = min;
-//        String label = formatter.format(labelVal);
-//        int x = LEFT_MARGIN;
-//        int y = getHeight() - BOTTOM_MARGIN + 12;
-//        for (int b: bins){
-//            g.drawString(label, x - 12, y);
-//            x += binWidth;
-//            labelVal += binSize;
-//            label = formatter.format(labelVal);
-//        }
-//        g.drawString(label, x - 12, y);
+        int bestWidth = (getWidth() - LEFT_MARGIN - RIGHT_MARGIN)/bins.length;
+
+
+        String label;
+        int x = LEFT_MARGIN;
+        int y = getHeight() - BOTTOM_MARGIN + 5;
+        for (int b: bins){
+            label = formatter.format(b);
+            g.drawString(label, x + 5, y - scaleY(b) - 12);
+            x += bestWidth;
+        }
     }
 
-    private Double scaleY (int val, Double ratio){
-        return 0.0;
+    private int scaleY (int val){
+        int effectiveArea = getHeight() - BOTTOM_MARGIN - TOP_MARGIN;
+        return (int)(((float)val / maxFreq) * effectiveArea);
     }
 }
--- src/MonteCarlo.java
+++ src/MonteCarlo.java
@@ -1,16 +1,26 @@
-import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
-
-import java.util.ArrayList;
+import javax.swing.*;
 
 public class MonteCarlo {
     public static void main(String args[]){
-        Simulation sim = new Simulation(100000, 11);
+        Simulation sim = new Simulation(1000000, 11);
         System.out.println(sim.toString());
-        for (int tot: sim.getBuckets()){
-            System.out.println(tot);
-        }
-        System.out.println(Metrics.verifyDistribution(sim.getArray(), 0.0, 1.0, 1.0));
-        System.out.println(Metrics.verifyDistribution(sim.getArray(), 0.0, 1.0, 2.0));
-        System.out.println(Metrics.verifyDistribution(sim.getArray(), 0.0, 1.0, 3.0));
+
+        // Verify that the simulation above follow the Normal Distribution
+        System.out.printf("The percentage of random variables that fall within 1 SD from the Mean is: %f%%\n",
+                Metrics.verifyDistribution(sim.getArray(), 0.0, 1.0, 1.0));
+        System.out.printf("The percentage of random variables that fall within 2 SD from the Mean is: %f%%\n",
+                Metrics.verifyDistribution(sim.getArray(), 0.0, 1.0, 2.0));
+        System.out.printf("The percentage of random variables that fall within 3 SD from the Mean is: %f%%\n",
+                Metrics.verifyDistribution(sim.getArray(), 0.0, 1.0, 3.0));
+
+        // Create the histogram using the simulation object instantiated earlier
+        Histogram h = new Histogram(sim);
+        JFrame visuals = new JFrame();
+
+        visuals.setTitle("Histogram of Normally Distributed Random Variables");
+        visuals.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        visuals.add(h);
+        visuals.setSize(1200, 800);
+        visuals.setVisible(true);
     }
 }
